2958926fe6694068ca330ac9b062ceb4229d6082
Kirill Groshelev <software.dev1988@gmail.com>
2024-02-13 15:03:10 +0300

    v 1.0.3 - небольшие исправления в 'Api.ts'

--- frontend/src/api/Api.ts
+++ frontend/src/api/Api.ts
@@ -18,6 +18,8 @@ const wait = (ms: number) => {
     return new Promise( (resolve) => {setTimeout(resolve, ms)});
 }
 
+console.log(process.env.REACT_APP_BACKEND_HOST)
+
 class WWIIAPIBackend {
     private baseUrl = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:8080/'
     private defaultHeaders = {}
